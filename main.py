942f7cb91b974511c5f2bef98e14b65b38963cf4
Ved Prakash <prakash18ved@gmail.com>
2021-02-18 10:51:12 +0800

    Added template for main script

---
+++ main.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Author: Ved Prakash
+# @Date:   2021-02-18 10:48:30
+# @Last Modified by:   Ved Prakash
+# @Last Modified time: 2021-02-18 10:50:03
+
+# Main Script to run for Questions 1 ans 2 in Outline
+
+import pandas as pd
